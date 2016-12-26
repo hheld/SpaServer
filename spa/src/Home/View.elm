6d934086814d53d45e80709f538b455ab7fa55fc
Harald Held <harald.held@gmail.com>
2016-12-26 12:11:59 +0100

    Added login as kind of widget to the home page.

--- spa/src/Home/View.elm
+++ spa/src/Home/View.elm
@@ -4,6 +4,7 @@ import Html exposing (Html, div, text, button)
 import Html.Events exposing (onClick)
 import Model exposing (Model)
 import Messages exposing (Msg(..))
+import Login.View exposing (loginPage)
 
 
 homePage : Model -> Html Msg
@@ -11,4 +12,5 @@ homePage model =
     div []
         [ text "This is the home page"
         , text ("User: " ++ (toString model.currentUser) ++ "; CSRF token: " ++ model.csrfToken)
+        , Html.map Messages.MsgForLogin (loginPage model.currentUser model.csrfToken)
         ]
